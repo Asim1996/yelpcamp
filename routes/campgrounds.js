960b950e1060dae494505b5878cd7dc8e3d4d976
Asim1996 <awesomeasim2596@gmail.com>
2019-02-04 20:18:12 +0530

    minor fix height and err flash

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -49,7 +49,6 @@ router.post("/",middleware.isLoggedIn, function(req, res){
     };
   geocoder.geocode(req.body.location, function (err, data) {
      if (err || !data.length) {
-      req.flash('error', 'Invalid address',err);
       return res.redirect('back');
     }
     var lat = data[0].latitude;
